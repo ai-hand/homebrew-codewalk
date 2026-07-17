0488fb3c0fbbcc414b51e2588e95275b53771227
ai-hand <ai-hand@users.noreply.github.com>
2026-07-18 00:54:06 +0900

    Update CodeWalk cask to v0.2.0

--- Casks/codewalk.rb
+++ Casks/codewalk.rb
@@ -1,5 +1,5 @@
 cask "codewalk" do
-  version "0.1.0"
+  version "0.2.0"
   sha256 :no_check
 
   url "https://github.com/ai-hand/codewalk-dist/releases/download/v#{version}/CodeWalk-#{version}-macos.tar.gz"
